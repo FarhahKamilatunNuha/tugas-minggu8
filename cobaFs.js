3582929f64c68b57760dd12547e9f3f1dbf5bb51
Farhah Kamilatun Nuha 36 <farhahkamilatunnuha4803@gmail.com>
2019-09-10 09:09:58 +0700

    first commit

---
+++ cobaFs.js
@@ -0,0 +1,20 @@
+//proses pemanggilan library fs
+const fs = require('fs')
+
+console.log('start reading a file...')
+
+//proses readfile text.txt menggunakan library js
+fs.readFile('text.txt', 'utf-8', (err, content) => {
+    //untuk handle error
+  if (err) {
+    console.log('error happened during reading the file')
+    return console.log(err)
+  }
+
+  //lakukan sesuatu disini kalau berhasil
+
+  //console.log("yeay, success! Datanya bro:" + content);
+
+})
+
+console.log('end of the file')
